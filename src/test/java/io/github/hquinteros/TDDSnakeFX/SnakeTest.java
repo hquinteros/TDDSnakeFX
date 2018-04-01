4aec3a073126cf29f10359f3242f3174e4e7c0e2
henryq95 <quinteros7766@gmail.com>
2018-03-31 17:31:04 -0700

    cleaned up extra files and added junit-jupiter-params as a dependency. Added a Parameterized Test to test all snake moves.

--- src/test/java/io/github/hquinteros/TDDSnakeFX/SnakeTest.java
+++ src/test/java/io/github/hquinteros/TDDSnakeFX/SnakeTest.java
@@ -2,8 +2,8 @@ package io.github.hquinteros.TDDSnakeFX;
 
 import javafx.geometry.Point2D;
 import org.junit.jupiter.api.Test;
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
@@ -20,5 +20,17 @@ class SnakeTest {
         snake.update();
 
         assertThat(snake.getPosition(), is(new Point2D(1, 0)));
+    }
+
+    @ParameterizedTest
+    @EnumSource(Direction.class)
+    void testAllSnakeMoves(Direction direction){
+        Snake snake = new Snake(new Point2D(0, 0));
+
+        snake.setDirection(direction);
+
+        snake.update();
+
+        assertThat(snake.getPosition(), is(direction.vector));
     }
 }
